0957d50df458facfa4a63da893033877f39bbf88
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-01-16 01:46:44 -0300

    delete duplicade files

--- src/components/Cell.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { StyledCell } from "./styles/StyledCell";
-import { TETROMINOS } from "../tetrominos";
-
-export default function Cell({ type }) {
-  return (
-    <StyledCell color={TETROMINOS[type].color} type={type} />
-  );
-}
